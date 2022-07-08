ae5b428c0c6c9aab995de0f3d55df48881971cbd
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-07-08 19:24:07 +0200

    Install Framer Motion, add animations to Home and NavBar Component

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,20 +3,42 @@ import styles from './NavBar.module.css';
 import { ReactComponent as Logo } from "../../Resources/image/logo.svg";
 import { ReactComponent as Browse } from "../../Resources/image/browse.svg";
 import { ReactComponent as Cart } from "../../Resources/image/cart.svg";
+import { ReactComponent as GitHub } from "../../Resources/image/github.svg";
+import { motion } from "framer-motion";
 
 const NavBar = props => {
-  const { browsing } = props;
+    const { browsing,
+            handleHover,
+            hoverState
+          } = props;
+    
+    const variants = {
+        hidden: { opacity: 1, y: 20 },
+        visible: { opacity: 1, y: 0 },
+    }
 
   return (
     <>
-      <div className={styles.navbar}>
+      <motion.div 
+        className={styles.navbar}
+        animate="visible"
+        initial="hidden"
+        variants={variants}
+        transition={{ y: { type: "spring" }, duration: 1 }}
+      >
         <div className={styles.navbar_left}>
-            <div className={styles.logodiv}>
-                <Logo className={styles.svg} />
-                <h3>Game Store</h3>
+            <div className={styles.logodiv} id="0"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+            >
+                <Logo className={styles.svg} style={{ fill: hoverState[0].hovered ? "#fff" : "#cccccc" }}/>
+                <h3 style={{ color: hoverState[0].hovered ? "#fff" : "#cccccc" }}>Game Store</h3>
             </div>
     
-            <div className={styles.pathdiv}>
+            <div className={styles.pathdiv} id="1"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+            >
                 {browsing ? 
                   <>
     
@@ -25,18 +47,31 @@ const NavBar = props => {
                   :
                   
                   <>
-                    <Browse className={styles.svg} />
-                    <h3>Browse Store</h3>
+                    <Browse className={styles.svg} style={{ fill: hoverState[1].hovered ? "#fff" : "#cccccc" }}/>
+                    <h3 style={{ color: hoverState[1].hovered ? "#fff" : "#cccccc" }}>Browse Store</h3>
                   </>
                 }
             </div>
         </div>
 
         <div className={styles.navbar_right}>
-          <Cart className={styles.svg} />
-          <h3>Cart: 0</h3>
+            <div className={styles.githubdiv} id="2"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+            >
+                <GitHub className={styles.gh} style={{ fill: hoverState[2].hovered ? "#fff" : "#cccccc" }}/>
+                <h3 style={{ color: hoverState[2].hovered ? "#fff" : "#cccccc" }}>GitHub</h3>
+            </div>
+
+            <div className={styles.cartdiv} id="3"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+            >
+                <Cart className={styles.svg} style={{ fill: hoverState[3].hovered ? "#fff" : "#cccccc" }}/>
+                <h3 style={{ color: hoverState[3].hovered ? "#fff" : "#cccccc" }}>Cart: 0</h3>
+            </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,55 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Home.module.css';
 import NavBar from '../../Components/NavBar/NavBar';
 import { ReactComponent as Credibility } from "../../Resources/image/credible.svg";
 import { ReactComponent as Availability } from "../../Resources/image/available.svg";
 import { ReactComponent as Reviews } from "../../Resources/image/reviews.svg";
 import { ReactComponent as Browse } from "../../Resources/image/browse.svg";
+import { hover } from '@testing-library/user-event/dist/hover';
+import { motion } from "framer-motion";
 
 const Home = () => {
+  const [hoverState, setHoverState] = useState([
+    {
+        hovered: false
+    },
+    {
+        hovered: false
+    },
+    {
+        hovered: false
+    },
+    {
+        hovered: false
+    },
+    {
+        hovered: false
+    },
+    {
+        hovered: false
+    },
+    {
+        hovered: false
+    }
+  ]);
+
+  const handleHover = (e) => {
+    let newHoverState = hoverState[e.target.id];
+    newHoverState.hovered = !newHoverState.hovered;
+
+    setHoverState([
+        ...hoverState, hoverState[e.target.id] = newHoverState
+    ]);
+  }
+
+  const variants = {
+    hidden: { opacity: 1, x: -100 },
+    visible: { opacity: 1, x: 0 },
+  }
+
+  const buttonVariants = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0 },
+  }
+
   return (
     <div className={styles.main}>
         <div className={styles.home}>
-            <NavBar />
+            <NavBar 
+              handleHover={handleHover} 
+              hoverState={hoverState}
+            />
     
-            <div className={styles.home_content}>
+            <motion.div 
+              className={styles.home_content}
+              initial="hidden"
+              animate="visible"
+              variants={variants}
+              transition={{ x: { type: "spring" }, duration: 1 }}
+            >
                 <h1>Game Store</h1>
                 <h2>„Heaven for gaming deals“ — IGN</h2>
-            </div>
+            </motion.div>
         </div>
 
-        <div className={styles.buttons}>
-            <button>
-                <Credibility className={styles.svg} />
+        <motion.div 
+          className={styles.buttons}
+          initial="hidden"
+          animate="visible"
+          variants={buttonVariants}
+          transition={{ x: { type: "spring" }, duration: 1.5 }}
+        >
+            <button id="4"
+                  onMouseEnter={handleHover}
+                  onMouseLeave={handleHover}
+            >
+                <Credibility className={styles.svg} style={{ fill: hoverState[4].hovered ? "#000000" : "#cccccc" }}/>
                 <div>
-                    <h3>Credible Shop</h3>
-                    <p>Excellenct Score</p>
+                    <h3 style={{ color: hoverState[4].hovered ? "#000000" : "#cccccc" }}>Credible Shop</h3>
+                    <p style={{ color: hoverState[4].hovered ? "#000000" : "#cccccc" }}>Excellenct Score</p>
                 </div>
             </button>
 
-            <button>
-                <Availability className={styles.svg} />
+            <button id="5"
+                onMouseEnter={handleHover}
+                onMouseLeave={handleHover}
+            >
+                <Availability className={styles.svg} style={{ fill: hoverState[5].hovered ? "#000000" : "#cccccc" }}/>
                 <div>
-                    <h3>High Availability</h3>
-                    <p>24-hour Support</p>
+                    <h3 style={{ color: hoverState[5].hovered ? "#000000" : "#cccccc" }}>High Availability</h3>
+                    <p style={{ color: hoverState[5].hovered ? "#000000" : "#cccccc" }}>24-hour Support</p>
                 </div>
             </button>
 
-            <button>
-                <Reviews className={styles.svg} />
+            <button id="6"
+                onMouseEnter={handleHover}
+                onMouseLeave={handleHover}
+            >
+                <Reviews className={styles.svg} style={{ fill: hoverState[6].hovered ? "#000000" : "#cccccc" }}/>
                 <div>
-                    <h3>Great Reviews</h3>
-                    <p>9.4 / 10 Avg. Score</p>
+                    <h3 style={{ color: hoverState[6].hovered ? "#000000" : "#cccccc" }}>Great Reviews</h3>
+                    <p style={{ color: hoverState[6].hovered ? "#000000" : "#cccccc" }}>9.4 / 10 Avg. Score</p>
                 </div>
             </button>
-        </div>
+        </motion.div>
 
-        <div className={styles.home_cred}>
+        <motion.div 
+          className={styles.home_cred}
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          transition={{ x: { type: "spring" }, duration: 1 }}
+        >
             <button>
-                <Browse />
+                <Browse className={styles.browse} />
                 Browse
             </button>
-        </div>
+        </motion.div>
     </div>
   );
 }
