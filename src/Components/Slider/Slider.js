f20a11e5421d6231a3f0060f06b9b0e5b200c29a
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-07-22 17:57:23 +0200

    Add mobile gestures to slider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -5,6 +5,8 @@ import { ReactComponent as Left } from "../../Resources/image/left.svg";
 import { ReactComponent as Right } from "../../Resources/image/right.svg";
 import { ReactComponent as Dot } from "../../Resources/image/dot.svg";
 import { useLocation } from 'react-router-dom';
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
 
 const Slider = props => {
   const {
@@ -20,48 +22,67 @@ const Slider = props => {
   } = props;
 
   const [footageIndex, setFootageIndex] = useState(0);
+  const slideRef = React.createRef();
   const location = useLocation();
+  let indicatorList = document.getElementsByClassName('indicator');
 
   useEffect(() => {
     const selectedGameIndex = allGames.findIndex(game => "/" + game.surname === location.pathname);
     setSelectedGame(allGames[selectedGameIndex]);
   }, []);
 
-  return (
+  const properties = {
+    duration: 6000,
+    autoplay: false,
+    transitionDuration: 800,
+    arrows: false,
+    infinite: true,
+    easing: "ease",
+    indicators: (i) => <div className={styles.indicator}>
+                           {i + 1}
+                       </div>
+  };
 
+  const slideImages = [
+    selectedGame.footage[0],
+    selectedGame.footage[1],
+    selectedGame.footage[2],
+    selectedGame.footage[3]
+  ];
+
+  const back = () => {
+    slideRef.current.goBack();
+  }
+
+  const next = () => {
+    slideRef.current.goNext();
+  }
+
+  return (
         <div className={styles.slider}>
-            <img 
-              className={styles.currentImg} 
-              src={selectedGame.footage[carouselState]}
-            />
+          <Slide ref={slideRef} {...properties}>
+            {slideImages.map((each, index) => (
+              <div key={index} className={styles.slide}>
+                <img className={styles.currentImg} src={each} alt="sample" />
+              </div>
+            ))}
+          </Slide>
     
-            <button className={styles.backwards} onClick={decrementCarousel} id="22" onMouseEnter={handleHover} onMouseLeave={handleHover}>
+            <button className={styles.backwards} onClick={back} id="22" onMouseEnter={handleHover} onMouseLeave={handleHover}>
                 <Left 
                   className={styles.left} 
                   style={{ fill: hoverState[22].hovered ? "#fff" : "#ccc" }}
                 />
             </button>
     
-            <button className={styles.forward} onClick={incrementCarousel} id="23" onMouseEnter={handleHover} onMouseLeave={handleHover}>
+            <button className={styles.forward} onClick={next} id="23" onMouseEnter={handleHover} onMouseLeave={handleHover}>
                 <Right
                   className={styles.right} 
                   style={{ fill: hoverState[23].hovered ? "#fff" : "#ccc" }}
                 />
             </button>
-    
             <div className={styles.selectorContainer}>
-                <button id="0" onClick={(e) => setCarouselState(0)}>
-                    <Dot className={carouselState === 0 ? styles.dotSelected : styles.dot} />
-                </button>
-                <button id="1" onClick={(e) => setCarouselState(1)}>
-                    <Dot className={carouselState === 1 ? styles.dotSelected : styles.dot} />
-                </button>
-                <button id="2" onClick={(e) => setCarouselState(2)}>
-                    <Dot className={carouselState === 2 ? styles.dotSelected : styles.dot} />
-                </button>
-                <button id="3" onClick={(e) => setCarouselState(3)}>
-                    <Dot className={carouselState === 3 ? styles.dotSelected : styles.dot} />
-                </button>
+
             </div>
         </div>
   );
