4223c921cdb3255bd3e25b6af5fa164daaca3522
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-07-22 17:58:52 +0200

    Remove unused imports and stylerules from GamePage

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from "framer-motion";
 import { ReactComponent as Left } from "../../Resources/image/left.svg";
 import { ReactComponent as Right } from "../../Resources/image/right.svg";
-import { ReactComponent as Dot } from "../../Resources/image/dot.svg";
 import { useLocation } from 'react-router-dom';
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
