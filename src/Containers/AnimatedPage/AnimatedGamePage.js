f777597c7d3cfe28507046401ad2e974ec4b58e2
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-08-08 20:54:38 +0200

    Reduce animation duration for page transitions

--- src/Containers/AnimatedPage/AnimatedGamePage.js
+++ src/Containers/AnimatedPage/AnimatedGamePage.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const animations = {
     initial: { opacity: 0, x: -200 },
-    animate: { opacity: 1, x: 0, transition: { x: { type: "spring", duration: 1, bounce: 0.4 }} },
-    exit: { opacity: 0, x: -200, transition: { x: { type: "tween", duration: 0.5, bounce: 0.3 }} },
+    animate: { opacity: 1, x: 0, transition: { x: { type: "spring", duration: 0.9, bounce: 0.4 }} },
+    exit: { opacity: 0, x: -200, transition: { x: { type: "tween", duration: 0.4, bounce: 0.3 }} },
 }
 
 const AnimatedGamePage = ({children}) => {
